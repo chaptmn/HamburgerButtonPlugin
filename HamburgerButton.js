c78f94384d102d57a1d68fbc8e5b84e56888809f
Naoya <loutihuji@icloud.com>
2016-04-30 02:38:04 +0900

    fixed

--- HamburgerButton.js
+++ HamburgerButton.js
@@ -1,10 +1,24 @@
 
 $(function(){
-  $('#HamburgerMenu').hide();
-  $('.Ham').click(function(){
-   if( $('#HamburgerMenu').css('display') == 'none') {
-       $('#HamburgerMenu').slideDown();}
-       else{
-       $('#HamburgerMenu').slideUp();}
-	});
+
+ var Hamburger = function(){
+
+   var _HamburgerMenu = $('#HamburgerMenu'),
+       _Ham = $('.Ham'),
+       _HambEvent;
+
+   _HamburgerMenu.hide();
+
+  _HambEvent = function(){
+    _Ham.click(function(){
+      if(_HamburgerMenu.css('display') == 'none') {
+          _HamburgerMenu.slideDown()
+         }else{
+          _HamburgerMenu.slideUp();}
+        });
+    };
+    _HambEvent();
+ };
+ Hamburger();
+
 });
